b428116fb88d94e94f35a23aa2269670a5254d48
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-10-25 16:37:28 -0400

    add edit and delete button functionality

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,45 +1,102 @@
-const renderTasks = function (taskObjArray) {
-  for (let task of taskObjArray) {
-    let layout = createTweetElement(task);
-    $("#list-section").prepend(layout);
-  }
-};
+// $("form").on("submit", (event) => {
+//   event.preventDefault();
+//   const $taskText = $("#input-task").val();
+//   const $taskCategory = $("#select-category").val()
+//   const $taskDate = $("#input-date").val()
+//   const $taskPriority = $("#select-priority").val()
 
-$("form").on("submit", (event) => {
-  event.preventDefault();
-  const $taskText = $("#input-task").val();
-  const $taskCategory = $("#select-category").val()
-  const $taskDate = $("#input-date").val()
-  const $taskPriority = $("#select-priority").val()
+//   const $task =
+//     `<article class="task task-${$taskCategory}">
+//   <div>
+//     <h2>${$taskText}</h2>
+//     <p>${$taskDate}</p>
+//   </div>
+//   <div class="task-icon-container">
+//     <div class="dot ${$taskPriority}"></div>
+//     <div>
+//       <i class="task-icon fa-solid fa-square-check"></i>
+//       <i class="task-icon fa-solid fa-pencil"></i>
+//       <i class="task-icon fa-solid fa-trash"></i>
+//     </div>
+//   </div>
+// </article>`
 
-  const $task =
-    `<article class="task task-${$taskCategory}">
-  <div>
-    <h2>${$taskText}</h2>
-    <p>${$taskDate}</p>
-  </div>
-  <div class="task-icon-container">
-    <div class="dot ${$taskPriority}"></div>
-    <div>
-      <i class="task-icon fa-solid fa-square-check"></i>
-      <i class="task-icon fa-solid fa-pencil"></i>
-      <i class="task-icon fa-solid fa-trash"></i>
-    </div>
-  </div>
-</article>`
+// $("#list-section").prepend($task)
 
-$("#list-section").prepend($task)
+// $("form")[0].reset();
+// });
 
-console.log($taskCategory)
+let taskToEdit = null; // Store the task being edited
 
-$("form")[0].reset();
+// When .fa-pencil is clicked, set the task to edit and pre-fill the form
+$("#list-section").on('click', '.fa-pencil', function (event) {
+  openModal();
+
+  // Store the task being edited for later reference
+  taskToEdit = $(event.target).closest('.task');
+  
+  // Pre-fill the form with the task details
+  const taskTitle = taskToEdit.find('h2').text();
+  const taskCategory = taskToEdit.hasClass('task-watch') ? 'watch' :
+                      taskToEdit.hasClass('task-eat') ? 'eat' :
+                      taskToEdit.hasClass('task-read') ? 'read' : 'buy';
+  const taskDate = taskToEdit.find('p').text();
+  const taskPriority = taskToEdit.find('.dot').attr('class').replace('dot', '').trim();
+  
+  $("#input-task").val(taskTitle);
+  $("#select-category").val(taskCategory);
+  $("#input-date").val(taskDate);
+  $("#select-priority").val(taskPriority);
 });
 
-const $taskIcon = $(".fa-square-check")
+// When the form is submitted, update the task
+$("form").on("submit", (event) => {
+  event.preventDefault();
+  
+  const $taskText = $("#input-task").val();
+  const $taskCategory = $("#select-category").val();
+  const $taskDate = $("#input-date").val();
+  const $taskPriority = $("#select-priority").val();
+  
+  if (taskToEdit) {
+    // Update the existing task with new values
+    taskToEdit.find('h2').text($taskText);
+    taskToEdit.removeClass().addClass(`task task-${$taskCategory}`);
+    taskToEdit.find('p').text($taskDate);
+    taskToEdit.find('.dot').attr('class', `dot ${$taskPriority}`);
+  } else {
+    // If taskToEdit is null, it means a new task is being created
+    const $task =
+      `<article class="task task-${$taskCategory}">
+    <div>
+      <h2>${$taskText}</h2>
+      <p>${$taskDate}</p>
+    </div>
+    <div class="task-icon-container">
+      <div class="dot ${$taskPriority}"></div>
+      <div>
+        <i class="task-icon fa-solid fa-square-check"></i>
+        <i class="task-icon fa-solid fa-pencil"></i>
+        <i class="task-icon fa-solid fa-trash"></i>
+      </div>
+    </div>
+  </article>`;
+    
+    $("#list-section").prepend($task);
+  }
+  
+  $("form")[0].reset();
+  taskToEdit = null; // Reset the taskToEdit variable
+});
 
-$taskIcon.on('click', (event) => {
-  event.target.classList.toggle("clicked")
-})
+$("#list-section").on('click', '.fa-trash', function (event) {
+  const taskToDelete = $(event.target).closest('.task');
+  taskToDelete.remove();
+});
+
+$("#list-section").on('click', '.fa-square-check', function (event) {
+  $(event.target).toggleClass("clicked");
+});
 
 $("#nav-all").on("click", (event) => {
   const $task = $(".task");
