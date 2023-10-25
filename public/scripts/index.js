3ed9b72dc5a1d399203fbb27778f5d7c439a26f2
Ahmed A <a.abdi11965@gmail.com>
2023-10-25 17:30:34 -0400

    Updated Css, HTML, and EJS files

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,71 +1,127 @@
+// Store the task being edited
+let taskToEdit = null;
+
+// Edit task when .fa-pencil is clicked
+$("#list-section").on('click', '.fa-pencil', function (event) {
+  openModal();
+
+  // Store the task being edited for later reference
+  taskToEdit = $(event.target).closest('.task');
+
+  // Pre-fill the form with the task details
+  const taskTitle = taskToEdit.find('h2').text();
+  const taskCategory = taskToEdit.hasClass('task-watch') ? 'watch' :
+    taskToEdit.hasClass('task-eat') ? 'eat' :
+      taskToEdit.hasClass('task-read') ? 'read' : 'buy';
+  const taskDate = taskToEdit.find('p').text();
+  const taskPriority = taskToEdit.find('.dot').attr('class').replace('dot', '').trim();
+
+  $("#input-task").val(taskTitle);
+  $("#select-category").val(taskCategory);
+  $("#input-date").val(taskDate);
+  $("#select-priority").val(taskPriority);
+});
+
+// Update the task when the form is submitted
 $("form").on("submit", (event) => {
-    event.preventDefault();
-    const $taskText = $("#input-task");
-    const $taskCategory = $("#select-category")
-    const $taskDate = $("#input-date")
-    const $taskPriority = $("#select-priority")
-    
-    
-    
-    console.log("TASK", $taskText.val())
-    console.log("CAT", $taskCategory.val())
-    console.log("DATE", $taskDate.val())
-    console.log("PRIORITY", $taskPriority.val())
-    $("form")[0].reset();
-    });
-    
-    const $taskIcon = $('.fa-square-check')
-    
-    $("#nav-all").on("click", (event) => {
-      const $task = $(".task");
-          $task.removeClass("invisible")
-    })
-    
-    $("#nav-watch").on("click", (event) => {
-      const $task = $(".task");
-      $task.each((index, element) => {
-        if ($(element).hasClass("task-watch")) {
-          $(element).removeClass("invisible")
-        } else {
-          $(element).addClass("invisible")
-        }
-      })
-    })
-    
-    $("#nav-eat").on("click", (event) => {
-      const $task = $(".task");
-      $task.each((index, element) => {
-        if ($(element).hasClass("task-eat")) {
-          $(element).removeClass("invisible")
-        } else {
-          $(element).addClass("invisible")
-        }
-      })
-    })
-    
-    $("#nav-read").on("click", (event) => {
-      const $task = $(".task");
-      $task.each((index, element) => {
-        if ($(element).hasClass("task-read")) {
-          $(element).removeClass("invisible")
-        } else {
-          $(element).addClass("invisible")
-        }
-      })
-    })
-    
-    $("#nav-buy").on("click", (event) => {
-      const $task = $(".task");
-      $task.each((index, element) => {
-        if ($(element).hasClass("task-buy")) {
-          $(element).removeClass("invisible")
-        } else {
-          $(element).addClass("invisible")
-        }
-      })
-    })
-    
-    $taskIcon.on('click', (event) => {
-      event.target.classList.toggle("clicked")
-    })
-    
+  event.preventDefault();
+
+  const $taskText = $("#input-task").val();
+  const $taskCategory = $("#select-category").val();
+  const $taskDate = $("#input-date").val();
+  const $taskPriority = $("#select-priority").val();
+
+  if (taskToEdit) {
+    // Update the existing task with new values
+    taskToEdit.find('h2').text($taskText);
+    taskToEdit.removeClass().addClass(`task task-${$taskCategory}`);
+    taskToEdit.find('p').text($taskDate);
+    taskToEdit.find('.dot').attr('class', `dot ${$taskPriority}`);
+  } else {
+    // Create a new task if taskToEdit is null
+    const $task = `<article class="task task-${$taskCategory}">
+    <div>
+      <h2>${$taskText}</h2>
+      <p>${$taskDate}</p>
+    </div>
+    <div class="task-icon-container">
+      <div class="dot ${$taskPriority}"></div>
+      <div>
+        <i class="task-icon fa-solid fa-square-check"></i>
+        <i class="task-icon fa-solid fa-pencil"></i>
+        <i class="task-icon fa-solid fa-trash"></i>
+      </div>
+    </div>
+  </article>`;
+
+    $("#list-section").prepend($task);
+  }
+
+  $("form")[0].reset();
+  taskToEdit = null; // Reset the taskToEdit variable
+});
+
+// Delete a task when .fa-trash is clicked
+$("#list-section").on('click', '.fa-trash', function (event) {
+  const taskToDelete = $(event.target).closest('.task');
+  taskToDelete.remove();
+});
+
+// Toggle task completion when .fa-square-check is clicked
+$("#list-section").on('click', '.fa-square-check', function (event) {
+  $(event.target).toggleClass("clicked");
+});
+
+// Show all tasks
+$("#nav-all").on("click", (event) => {
+  const $task = $(".task");
+  $task.removeClass("invisible");
+});
+
+// Show watch tasks
+$("#nav-watch").on("click", (event) => {
+  const $task = $(".task");
+  $task.each((index, element) => {
+    if ($(element).hasClass("task-watch")) {
+      $(element).removeClass("invisible");
+    } else {
+      $(element).addClass("invisible");
+    }
+  });
+});
+
+// Show eat tasks
+$("#nav-eat").on("click", (event) => {
+  const $task = $(".task");
+  $task.each((index, element) => {
+    if ($(element).hasClass("task-eat")) {
+      $(element).removeClass("invisible");
+    } else {
+      $(element).addClass("invisible");
+    }
+  });
+});
+
+// Show read tasks
+$("#nav-read").on("click", (event) => {
+  const $task = $(".task");
+  $task.each((index, element) => {
+    if ($(element).hasClass("task-read")) {
+      $(element).removeClass("invisible");
+    } else {
+      $(element).addClass("invisible");
+    }
+  });
+});
+
+// Show buy tasks
+$("#nav-buy").on("click", (event) => {
+  const $task = $(".task");
+  $task.each((index, element) => {
+    if ($(element).hasClass("task-buy")) {
+      $(element).removeClass("invisible");
+    } else {
+      $(element).addClass("invisible");
+    }
+  });
+});
--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -2,31 +2,44 @@ const $modal = $(".modal");
 const $overlay = $(".overlay");
 const $openModalBtn = $(".btn-open");
 const $closeModalBtn = $(".btn-close");
-const $editIcon = $(".fa-pencil")
+const $editIcon = $(".fa-pencil");
 
+// Function to open the modal
 const openModal = function () {
   $modal.removeClass("hidden");
   $overlay.removeClass("hidden");
 };
 
+// Event handler to open the modal when the "Add Task" button is clicked
+$("#add-task").on("click", () => {
+  // Clear the form inputs
+  $("#input-task").val("");
+  $("#select-category").val("");
+  $("#input-date").val("");
+  $("#select-priority").val("");
+
+  openModal();
+});
+
+// Function to close the modal
 const closeModal = function () {
   $modal.addClass("hidden");
   $overlay.addClass("hidden");
 };
 
+// Event handlers for opening and closing the modal
 $openModalBtn.on("click", () => {
-  openModal()
+  openModal();
 });
 
-$editIcon.on("click", () => {
-  openModal()
-
-})
+$("#list-section").on('click', '.fa-pencil', function (event) {
+  openModal();
+});
 
 $closeModalBtn.on("click", () => {
-  closeModal()
+  closeModal();
 });
 
 $overlay.on("click", () => {
-  closeModal()
+  closeModal();
 });
