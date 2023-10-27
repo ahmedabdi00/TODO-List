ece49931089552a8b1ef06afc914cc036600539f
Ahmed A <a.abdi11965@gmail.com>
2023-10-27 00:30:32 -0400

    .

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,5 +1,5 @@
 -- Users table seeds here (Example)
 INSERT INTO users (name, email, password)
-VALUES ('Eva Stanley', 'test@gmail.ca', '$2b$12$iW0/IT5VD5QT9X9DBcISEufEF7I816pYurdrq7slt.MRXV2kpBZnO'),
+VALUES ('Eva Stanley', 'sebastianguerra@ymail.com', '$2b$12$iW0/IT5VD5QT9X9DBcISEufEF7I816pYurdrq7slt.MRXV2kpBZnO'),
 ('Louisa Meyer', 'jacksonrose@hotmail.com', '$2b$12$iW0/IT5VD5QT9X9DBcISEufEF7I816pYurdrq7slt.MRXV2kpBZnO'),
 ('Dominic Parks', 'victoriablackwell@outlook.com', '$2b$12$iW0/IT5VD5QT9X9DBcISEufEF7I816pYurdrq7slt.MRXV2kpBZnO');
